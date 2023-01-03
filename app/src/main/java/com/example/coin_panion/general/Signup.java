eace3b419764d63b501f09c15b9059d4762afad6
Nazrin <mohammadnazrin.edu@gmail.com>
2023-01-04 00:33:44 +0800

    Added database connection for verifying login credentials. Created Signup.java to handle signup processes

---
+++ app/src/main/java/com/example/coin_panion/general/Signup.java
@@ -0,0 +1,14 @@
+package com.example.coin_panion.general;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
