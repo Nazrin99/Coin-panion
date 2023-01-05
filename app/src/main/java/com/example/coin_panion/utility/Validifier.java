58e7cc7a0077f485d802583c1cfecee525689bf7
Nazrin <mohammadnazrin.edu@gmail.com>
2023-01-06 04:04:17 +0800

    Moved methods used for verification into Validifier.java to improve modularity and reusability. Created method to send SMS

---
+++ app/src/main/java/com/example/coin_panion/utility/Validifier.java
@@ -0,0 +1,26 @@
+package com.example.coin_panion.utility;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
+
+public class Validifier {
+    public static boolean isEmail(String email){
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    public static boolean isPhoneNumber(String phoneNumber){
+        // Definitions of a phone number
+        //      +601xxxxxxxx[x?]
+        // Prefix +601 followed by 8 or 9 numbers
+
+        String pattern = "[+]601[0-9]{8}[0-9]?";
+        return (!TextUtils.isEmpty(phoneNumber) && Pattern.compile(pattern).matcher(phoneNumber).matches());
+    }
+
+    public static boolean validPassword(String password){
+        return (password.length() >= 8 && password.matches(".*[^a-zA-Z0-9 ].*") && password.matches(".*\\d+.*"));
+    }
+}
