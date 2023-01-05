2189cab98da68218fad888135b547b4e2d7efbc9
iffah <u2001282@siswa.um.edu.my>
2023-01-05 23:55:30 +0800

    UI updates

--- app/src/main/java/com/example/coin_panion/BlockAccountActivity.java
+++ app/src/main/java/com/example/coin_panion/BlockAccountActivity.java
@@ -2,19 +2,19 @@ package com.example.coin_panion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class EditProfileActivity extends AppCompatActivity {
+public class BlockAccountActivity extends AppCompatActivity {
 
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.activity_block_account);
 
         textView = findViewById(R.id.activity_name);
-        textView.setText("EDIT PROFILE");
+
+        textView.setText("BLOCKED ACCOUNTS");
     }
 }
--- app/src/main/java/com/example/coin_panion/PrivacyActivity.java
+++ app/src/main/java/com/example/coin_panion/PrivacyActivity.java
@@ -2,19 +2,18 @@ package com.example.coin_panion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class EditProfileActivity extends AppCompatActivity {
+public class PrivacyActivity extends AppCompatActivity {
 
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.activity_privacy);
 
         textView = findViewById(R.id.activity_name);
-        textView.setText("EDIT PROFILE");
+        textView.setText("PRIVACY");
     }
 }
