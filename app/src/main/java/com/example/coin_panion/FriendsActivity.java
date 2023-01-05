b881fe4b5353f28dbe19001ce9a5321531d2e3c0
Afiq <arefiqssss@gmail.com>
2023-01-06 02:00:27 +0800

    Friends UI update, with navigation graph

---
+++ app/src/main/java/com/example/coin_panion/FriendsActivity.java
@@ -0,0 +1,23 @@
+package com.example.coin_panion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+//        NavHostFragment navHostFragment = findViewById(R.id.navHostFragment);
+//        NavController navController = navHostFragment.getNavController();
+//
+//        if (user has at least one friend){
+//            navController.navigate(R.id.friendsFragment);
+//        }
+    }
+}
