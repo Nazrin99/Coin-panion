6c163eb966d6f4599469cd466b94ef967104a113
Nazrin <u2000443@siswa.um.edu.my>
2023-01-07 18:44:51 +0800

    Updated User class and ThreadStatic.java

--- app/src/main/java/com/example/coin_panion/classes/utility/ThreadStatic.java
+++ app/src/main/java/com/example/coin_panion/classes/utility/ThreadStatic.java
@@ -2,6 +2,9 @@ package com.example.coin_panion.classes.utility;
 
 public class ThreadStatic {
     public static void run(Thread thread){
+        while(thread.isAlive()){
+
+        }
         thread.start();
         while(thread.isAlive()){
 
