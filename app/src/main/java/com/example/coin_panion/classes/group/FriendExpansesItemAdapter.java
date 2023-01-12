ed8a88f0c69c2c7386532276892e0085b40c890f
aaron <u2102810@siswa.um.edu.my>
2023-01-12 17:35:23 +0800

    Adapter

--- app/src/main/java/com/example/coin_panion/classes/group/FriendExpansesItemAdapter.java
+++ app/src/main/java/com/example/coin_panion/classes/group/FriendExpansesItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.coin_panion.classes.group;
 
 import android.content.Context;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +17,16 @@ import com.example.coin_panion.R;
 import com.example.coin_panion.classes.friends.ContactAdapter;
 import com.example.coin_panion.classes.general.User;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class FriendExpansesItemAdapter extends RecyclerView.Adapter<FriendExpansesItemAdapter.ViewHolder> {
 
     Context context;
     List<User> allGroupMembers;
-    List<User> selectedGroupMembers;
+    HashMap<Integer, Double> selectedGroupMembers;
 
-    public FriendExpansesItemAdapter(Context context, List<User> allGroupMembers, List<User> selectedGroupMembers) {
+    public FriendExpansesItemAdapter(Context context, List<User> allGroupMembers, HashMap<Integer, Double> selectedGroupMembers) {
         this.context = context;
         this.allGroupMembers = allGroupMembers;
         this.selectedGroupMembers = selectedGroupMembers;
@@ -50,15 +52,16 @@ public class FriendExpansesItemAdapter extends RecyclerView.Adapter<FriendExpans
         /*Initialze the variable based on Friend Expanses*/
         holder.TVCustomExpansesFriendName.setText(user.getFirstName());
 
-        Integer expansesAmount = Integer.valueOf(holder.ETCustomExpansesItemAmount.getText().toString().replaceAll("[^0-9]", ""));
+//        Integer expansesAmount = Integer.valueOf(holder.ETCustomExpansesItemAmount.getText().toString().replaceAll("[^0-9]", ""));
 
         holder.CBCustomExpansesItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    selectedGroupMembers.add(user);
+                    Double amount = Double.parseDouble(holder.ETCustomExpansesItemAmount.getText().toString());
+                    selectedGroupMembers.put(user.getUserID(), amount);
                 }else {
-                    selectedGroupMembers.remove(user);
+                    selectedGroupMembers.remove(user.getUserID());
                 }
             }
         });
@@ -85,5 +88,29 @@ public class FriendExpansesItemAdapter extends RecyclerView.Adapter<FriendExpans
 
         }
 
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public List<User> getAllGroupMembers() {
+        return allGroupMembers;
+    }
+
+    public void setAllGroupMembers(List<User> allGroupMembers) {
+        this.allGroupMembers = allGroupMembers;
+    }
+
+    public HashMap<Integer, Double> getSelectedGroupMembers() {
+        return selectedGroupMembers;
+    }
+
+    public void setSelectedGroupMembers(HashMap<Integer, Double> selectedGroupMembers) {
+        this.selectedGroupMembers = selectedGroupMembers;
     }
 }
